f8ef5defa6a76cdb931f7f0b7a3e90c06970477a
Rodyarad <r.vakhitov2018@gmail.com>
2025-01-28 14:22:54 +0300

    fixed bash scripts

--- scripts/train_oc_shapes2d.sh
+++ scripts/train_oc_shapes2d.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ex
 export OMP_NUM_THREADS=1
+export CUDA_DEVICE_ORDER=PCI_BUS_ID
 export CUDA_VISIBLE_DEVICES=0
 export HYDRA_FULL_ERROR=1
 python ez/train.py exp_config=ez/config/exp/oc_shapes2d.yaml
