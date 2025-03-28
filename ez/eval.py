e6ef3b8bed940b6d1ff1718df112d6b4ac562f84
Rodyarad <r.vakhitov2018@gmail.com>
2025-03-28 19:15:22 +0300

    check success rate

--- ez/eval.py
+++ ez/eval.py
@@ -144,7 +144,7 @@ def eval(agent, model, n_episodes, save_path, config, max_steps=None, use_pb=Fal
             # rewards[i].append(reward)
             rewards[i].append(info['raw_reward'])
             dones[i] = done
-            if info['is_success']:
+            if info['is_success'] and dones[i]:
                 success_rate+=1/n_episodes
 
 
